13e88e99963a8c245d6fdc5909de446c8f54ae27
anas-kirsch <anas83kirsch@gmail.com>
2025-03-07 15:52:36 +0100

    maj

--- TodoList/src/view/reloadTasks.js
+++ TodoList/src/view/reloadTasks.js
@@ -1,3 +1,4 @@
+import { Components } from "../components/Components.js";
 import { Local } from "../services/dataBase/local.js";
 import { display } from "./Display.js"; 
 
@@ -6,17 +7,24 @@ export function reloadTasks(){
     
 
     let tabTasksList = Local.getAllTasks();
-    tabTasksList.forEach(element => {
-        const sectionNouvelleTache = document.querySelectorAll(".ListTask");
-        const divTacheContent = document.querySelectorAll(".display-Task-Content");
 
-        sectionNouvelleTache.forEach(element => {
-            element.remove();
-        });
-        divTacheContent.forEach(element => {
-            element.remove();
-        });
-    });
+    //ca 
+
+    // tabTasksList.forEach(element => {
+    //     const sectionNouvelleTache = document.querySelectorAll(".ListTask");
+    //     const divTacheContent = document.querySelectorAll(".display-Task-Content");
+
+    //     sectionNouvelleTache.forEach(element => {
+    //         element.remove();
+    //     });
+    //     divTacheContent.forEach(element => {
+    //         element.remove();
+    //     });
+    // });
+
+
+    //ou ca 
+    Components.DisplayTask.containerList.innerHTML = "";
 
 
 
--- TodoList/src/view/triDate.js
+++ TodoList/src/view/triDate.js
@@ -3,7 +3,7 @@ import { Local } from "../services/dataBase/local.js";
 import { reloadTasks } from "../view/reloadTasks.js";
 import { display } from "./Display.js";
 
-export function triDate(){
+export function triDate(tabInverser){
 
     Components.boutton.bouttontri.addEventListener('click',()=>{
 
@@ -13,43 +13,33 @@ export function triDate(){
 
         console.log(tabTasksList);
         
-        // tabTasksList.sort((a ,b) => {
+        const tabInverser = tabTasksList.sort((a ,b) => {
             
-            //     // console.log(a.date)
-            
-            //     return a.date - b.date;
-    // });
-    // console.log(tabTasksList);
-    // reloadTasks(tabTasksList);
-    
-    const ordre = tabTasksList.reverse();
-    console.log(ordre);
-    
+                // console.log(a.date)
+
+                return b.date - a.date;
+    });
 
     tabTasksList.forEach(element => {
         const sectionNouvelleTache = document.querySelectorAll(".ListTask");
         const divTacheContent = document.querySelectorAll(".display-Task-Content");
-
-        sectionNouvelleTache.forEach(element => {
-            element.remove();
-        });
-        divTacheContent.forEach(element => {
-            element.remove();
-        });
-    });
-
     
-        ordre.forEach(element => {
+        Components.DisplayTask.containerList.innerHTML = "";
+        });
+    
+    
+        tabInverser.forEach(element => {
             display.displayMyTask(element);
             const balisePasTrouve = document.querySelector(".messageRechercheIntrouvable");
             balisePasTrouve.textContent = "";
         });
-    
-
-
-
 
 });
     
-    
 }
+
+
+
+
+
+
