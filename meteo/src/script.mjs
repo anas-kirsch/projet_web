a97ea4610685b7c7f7fe5b30e160610035d9ec0b
anas-kirsch <anas83kirsch@gmail.com>
2025-03-27 16:54:35 +0100

    l

--- meteo/src/script.mjs
+++ meteo/src/script.mjs
@@ -7,7 +7,7 @@ import { ChooseSuggestions } from "./chooseSuggestions.mjs";
 
 main();
 
-function main(){
+async function main(){
 
     const div = document.querySelector(".div-proposition-recherche")
     
@@ -22,6 +22,17 @@ function main(){
     handleSearchBarreEvent();
     ChooseSuggestions();
 
+    const maLoc = await fetch("https://api.locationiq.com/v1/autocomplete?key=pk.0873db43d16d82f5ba795ff21e7788c0&q=&limit=5&dedupe=1&",{
+    }).then(response => response.json())
+
+    
+    console.log(maLoc)
 
 
+
+
+   
+
+
+    
 }
